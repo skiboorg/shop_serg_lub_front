6873687753de480cfa088b57b7d5f41708fcf26c
xxx <hgfh@dfds.ddd>
2025-07-07 21:52:54 +0300

    fix

--- app/components/StyleCard.vue
+++ app/components/StyleCard.vue
@@ -3,10 +3,10 @@ defineProps(['image', 'title','url'])
 </script>
 
 <template>
-<div class="">
+<div class="flex flex-col items-start justify-between">
   <img class="block w-full h-auto object-cover mb-6" :src="image" alt="">
   <p class="uppercase font-semibold mb-3">{{title}}</p>
-  <NuxtLink :to="url">
+  <NuxtLink :to="url" class="block w-full">
     <Button fluid outlined severity="contrast" label="СМОТРЕТЬ ПРОДУКТЫ"/>
   </NuxtLink>
 
