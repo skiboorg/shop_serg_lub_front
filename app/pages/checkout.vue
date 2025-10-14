8dbeaca88eb722f36b70f41961618e603a149917
xxx <hgfh@dfds.ddd>
2025-10-14 13:54:21 +0300

    fix

--- app/pages/checkout.vue
+++ app/pages/checkout.vue
@@ -26,6 +26,7 @@ const order_data = ref({
   phone:null,
   email:null,
   order_id:null,
+  sdek_at_door:false,
 })
 
 const validateEmail = computed(()=>{
@@ -37,8 +38,8 @@ function isOrderDataValid() {
 
   // список полей, которые *обязательно должны быть заполнены*
   const requiredFields = [
-    'selected_address',
-    'selected_delivery',
+    // 'selected_address',
+    // 'selected_delivery',
     'street',
     'building_1',
     'building_2',
@@ -63,7 +64,7 @@ const total_order_price = computed(()=>{
 
 
 const can_pay = computed(()=>{
-  return validateEmail.value && isOrderDataValid && total_order_price.value > 0
+  return validateEmail.value && isOrderDataValid
 })
 const  handleSuccess= async () => {
 
@@ -178,65 +179,78 @@ const fiasSelected = async () => {
           <InputText fluid   v-model="order_data.comment" id="comment`"/>
           <label for="comment">Комментарий к доставке</label>
         </FloatLabel>
-        <p class="font-semibold text-[20px] mt-14 mb-5">Доставка</p>
-        <p class="mb-4 font-extralight">Для получения возможных вариантов доставки выберите ваш населенный пункт (город\поселок).<br>
-          Без выбора доставки оформить заказ невозможно.<br>
-          Если вы не видите возможные варианты доставки, то вы некорректно выбрали населенный пункт!<br>
-          Извиняемся, это не самая удобная система, но сервис для расчета доставки по другому не умеет :(</p>
-        <FloatLabel
-            class="mb-5"
-            variant="in"
-            :key="1"
-        >
-          <Select :loading="loading"
-                  v-model="order_data.selected_address"
-                  editable
-                  class="w-full"
-                  :options="fias_data"
-                  optionLabel="value"
-                  @update:modelValue="fiasSelected"
-                  @input="handleAddressInput" />
-          <label for="over_label">Населенный пункт *</label>
-        </FloatLabel>
 
-        <div v-if="delivery_data" class="grid grid-cols-2 md:grid-cols-4 gap-3">
-          <div class="border p-2 flex flex-col items-start justify-between h-[115px]  hover:shadow-lg  hover:cursor-pointer"
-               :class="selected_delivery===delivery ? 'bg-black text-white' : ''"
-               @click="selected_delivery=delivery"
-               v-for="delivery in delivery_data.city_info.deliveries">
-            <p>{{delivery.delivery_name}}</p>
-            <p>от {{delivery.min_cost}}</p>
+        <p class="font-semibold text-[20px] mt-14 mb-5">Тип доставки</p>
+        <div class="flex flex-wrap gap-4">
+          <div class="flex items-center gap-2">
+            <RadioButton v-model="order_data.sdek_at_door" inputId="sdek_at_door1" name="false" :value="false" />
+            <label for="sdek_at_door1">Доставка СДЕК</label>
           </div>
-        </div>
-        <div class="mt-4" v-if="selected_delivery">
-          <p class="mb-4 font-extralight">Для выбора пункта получения введите ближайший к вам адрес ПВЗ.Список адресов доступен на официальном сайте</p>
-          <FloatLabel
-              class="mb-5"
-              variant="in"
-              :key="1"
-          >
-            <Select :loading="loading"
-                    v-model="order_data.selected_delivery"
-                    class="w-full"
-                    filter
-                    :filter-fields="['address']"
-                    :options="selected_delivery.options"
-                    optionLabel="name"
+          <div class="flex items-center gap-2">
+            <RadioButton v-model="order_data.sdek_at_door" inputId="sdek_at_door2" name="true" :value="true" />
+            <label for="sdek_at_door2">Доставка СДЕК до двери</label>
+          </div>
 
-            >
-              <template #option="slotProps">
-                <div class="flex items-center">
-                  <div>Cтоимость {{slotProps.option.cost}}, дата {{slotProps.option.delivery_date}}<br>
-                    {{slotProps.option.address}}</div>
-                </div>
-              </template>
-            </Select>
-            <label for="over_label">Выберите пункт получения</label>
-          </FloatLabel>
-          <p v-if="order_data.selected_delivery?.comment_address">{{order_data.selected_delivery?.comment_address}}</p>
-          <p v-if="order_data.selected_delivery?.schedule">Время работы: {{order_data.selected_delivery?.schedule}}</p>
-          <p v-if="order_data.selected_delivery?.phone">Телефон: {{order_data.selected_delivery?.phone}}</p>
         </div>
+<!--        <p class="font-semibold text-[20px] mt-14 mb-5">Доставка</p>-->
+<!--        <p class="mb-4 font-extralight">Для получения возможных вариантов доставки выберите ваш населенный пункт (город\поселок).<br>-->
+<!--          Без выбора доставки оформить заказ невозможно.<br>-->
+<!--          Если вы не видите возможные варианты доставки, то вы некорректно выбрали населенный пункт!<br>-->
+<!--          Извиняемся, это не самая удобная система, но сервис для расчета доставки по другому не умеет :(</p>-->
+<!--        <FloatLabel-->
+<!--            class="mb-5"-->
+<!--            variant="in"-->
+<!--            :key="1"-->
+<!--        >-->
+<!--          <Select :loading="loading"-->
+<!--                  v-model="order_data.selected_address"-->
+<!--                  editable-->
+<!--                  class="w-full"-->
+<!--                  :options="fias_data"-->
+<!--                  optionLabel="value"-->
+<!--                  @update:modelValue="fiasSelected"-->
+<!--                  @input="handleAddressInput" />-->
+<!--          <label for="over_label">Населенный пункт *</label>-->
+<!--        </FloatLabel>-->
+
+<!--        <div v-if="delivery_data" class="grid grid-cols-2 md:grid-cols-4 gap-3">-->
+<!--          <div class="border p-2 flex flex-col items-start justify-between h-[115px]  hover:shadow-lg  hover:cursor-pointer"-->
+<!--               :class="selected_delivery===delivery ? 'bg-black text-white' : ''"-->
+<!--               @click="selected_delivery=delivery"-->
+<!--               v-for="delivery in delivery_data.city_info.deliveries">-->
+<!--            <p>{{delivery.delivery_name}}</p>-->
+<!--            <p>от {{delivery.min_cost}}</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="mt-4" v-if="selected_delivery">-->
+<!--          <p class="mb-4 font-extralight">Для выбора пункта получения введите ближайший к вам адрес ПВЗ.Список адресов доступен на официальном сайте</p>-->
+<!--          <FloatLabel-->
+<!--              class="mb-5"-->
+<!--              variant="in"-->
+<!--              :key="1"-->
+<!--          >-->
+<!--            <Select :loading="loading"-->
+<!--                    v-model="order_data.selected_delivery"-->
+<!--                    class="w-full"-->
+<!--                    filter-->
+<!--                    :filter-fields="['address']"-->
+<!--                    :options="selected_delivery.options"-->
+<!--                    optionLabel="name"-->
+
+<!--            >-->
+<!--              <template #option="slotProps">-->
+<!--                <div class="flex items-center">-->
+<!--                  <div>Cтоимость {{slotProps.option.cost}}, дата {{slotProps.option.delivery_date}}<br>-->
+<!--                    {{slotProps.option.address}}</div>-->
+<!--                </div>-->
+<!--              </template>-->
+<!--            </Select>-->
+<!--            <label for="over_label">Выберите пункт получения</label>-->
+<!--          </FloatLabel>-->
+<!--          <p v-if="order_data.selected_delivery?.comment_address">{{order_data.selected_delivery?.comment_address}}</p>-->
+<!--          <p v-if="order_data.selected_delivery?.schedule">Время работы: {{order_data.selected_delivery?.schedule}}</p>-->
+<!--          <p v-if="order_data.selected_delivery?.phone">Телефон: {{order_data.selected_delivery?.phone}}</p>-->
+<!--        </div>-->
 
       </div>
 
@@ -247,13 +261,13 @@ const fiasSelected = async () => {
             <p class="uppercase">Товаров на сумму</p>
             <p>{{cartCount.total_price}} ₽</p>
           </div>
-          <div class="flex justify-between mb-5 pb-5 border-b">
-            <p class="uppercase">Доставка</p>
-            <p>{{order_data.selected_delivery?.cost || 0}} ₽</p>
-          </div>
+<!--          <div class="flex justify-between mb-5 pb-5 border-b">-->
+<!--            <p class="uppercase">Доставка</p>-->
+<!--            <p>{{order_data.selected_delivery?.cost || 0}} ₽</p>-->
+<!--          </div>-->
           <div class="flex justify-between  mb-5">
             <p class="uppercase">Итого:</p>
-            <p>{{total_order_price}} ₽</p>
+            <p>{{cartCount.total_price}} ₽</p>
           </div>
           <p v-if="pay_error" class="text-red-500 font-bold">{{pay_error}}</p>
           <LifePayWidget v-if='cartCount.items_count > 0'
@@ -262,7 +276,7 @@ const fiasSelected = async () => {
                          :loading="loading"
                          @success="handleSuccess"
                          :disabled="can_pay"
-                         :cost="total_order_price"
+                         :cost="cartCount.total_price"
                          :phone="order_data.phone"
                          :email="order_data.email" />
 
